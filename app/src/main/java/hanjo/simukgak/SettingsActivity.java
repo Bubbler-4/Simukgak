55eadbf69ee9da1ae56bd694402d98a344bcd7be
ilbmk <ilbmk@postech.ac.kr>
2016-11-29 01:23:15 +0900

    a

--- app/src/main/java/hanjo/simukgak/SettingsActivity.java
+++ app/src/main/java/hanjo/simukgak/SettingsActivity.java
@@ -1,13 +1,17 @@
 package hanjo.simukgak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
 }
