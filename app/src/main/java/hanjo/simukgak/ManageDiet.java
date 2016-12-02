a1e3b808a0f782f973a75e3cbc7bc421937f02a2
ilbmk <ilbmk@postech.ac.kr>
2016-12-02 21:36:38 +0900

    식단 관리 임시저장

--- app/src/main/java/hanjo/simukgak/ManageDiet.java
+++ app/src/main/java/hanjo/simukgak/ManageDiet.java
@@ -6,26 +6,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class ManageDiet extends AppCompatActivity {
 
-    EditText num1, num2, num3, num4, num5, pro1, pro2, pro3, pro4, pro5;
-    Button btnShow;
+    private TextView num1, num2, num3, num4, num5, pro1, pro2, pro3, pro4, pro5;
+    private Button btnShow;
+    private FileManager fileManager;
+    private ArrayList<String> fileValues;
+    private ArrayList<String> restaurantName;
+    private int[] orderCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_diet);
+        pro1 = (TextView)findViewById(R.id.pro1);
+        num1 = (TextView)findViewById(R.id.num1);
+        pro2 = (TextView)findViewById(R.id.pro2);
+        num2 = (TextView)findViewById(R.id.num2);
+        pro3 = (TextView)findViewById(R.id.pro3);
+        num3 = (TextView)findViewById(R.id.num3);
+        pro4 = (TextView)findViewById(R.id.pro4);
+        num4 = (TextView)findViewById(R.id.num4);
+        pro5 = (TextView)findViewById(R.id.pro5);
+        num5 = (TextView)findViewById(R.id.num5);
 
-        pro1 = (EditText)findViewById(R.id.pro1);
-        num1 = (EditText)findViewById(R.id.num1);
-        pro2 = (EditText)findViewById(R.id.pro2);
-        num2 = (EditText)findViewById(R.id.num2);
-        pro3 = (EditText)findViewById(R.id.pro3);
-        num3 = (EditText)findViewById(R.id.num3);
-        pro4 = (EditText)findViewById(R.id.pro4);
-        num4 = (EditText)findViewById(R.id.num4);
-        pro5 = (EditText)findViewById(R.id.pro5);
-        num5 = (EditText)findViewById(R.id.num5);
         //여기서 제품 받아오면 될것 같군요
         btnShow = (Button)findViewById(R.id.show);
 
@@ -37,41 +45,37 @@ public class ManageDiet extends AppCompatActivity {
 
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(
-                            ManageDiet.this,
-                            ShowWebChartActivity.class);
-
-                    intent.putExtra("PRO1", getPro(pro1));
-                    intent.putExtra("NUM1", getNum(num1));
-                    intent.putExtra("PRO2", getPro(pro2));
-                    intent.putExtra("NUM2", getNum(num2));
-                    intent.putExtra("PRO3", getPro(pro3));
-                    intent.putExtra("NUM3", getNum(num3));
-                    intent.putExtra("PRO4", getPro(pro4));
-                    intent.putExtra("NUM4", getNum(num4));
-                    intent.putExtra("PRO5", getPro(pro5));
-                    intent.putExtra("NUM5", getNum(num5));
-
-                    startActivity(intent);
+                    fileManager = new FileManager(getApplicationContext(), "orderlist_info.txt");
+                    fileValues=fileManager.readFile();
+                    restaurantName = new ArrayList<>();
+                    String[] values;
+                    orderCount= new int[fileValues.size()];
+                    for(int i=0; i<fileValues.size(); i++)
+                    {
+                        orderCount[i]=0;
+                    }
+                    int index;
+                    for(int i=0; i<fileValues.size(); i++) {
+                        values = fileValues.get(i).split(",");
+                        index=checkRepeat(values[0], restaurantName);
+                        if(index==-1) {
+                            //TODO: 여기서 date 비교해노읍시다..
+                            restaurantName.add(values[0]);
+                        }
+                        else
+                        {
+                            orderCount[index]++;
+                        }
+                    }
                 }
-
             };
 
-    private int getNum(EditText editText){
+    private int checkRepeat(String name, ArrayList<String> restaurantName) {
 
-        int num = 0;
-
-        String stringNum = editText.getText().toString();
-        if(!stringNum.equals("")){
-            num = Integer.valueOf(stringNum);
+        for (int i = 0; i < restaurantName.size(); i++) {
+            if (name.equals(restaurantName.get(i)))
+                return i;
         }
-
-        return (num);
-    }
-    private String getPro(EditText editText){
-
-        String pro;
-        pro = editText.getText().toString();
-        return (pro);
+        return -1;
     }
 }
