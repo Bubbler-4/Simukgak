b1aec009e33a9cee6c4dfea04822cba73a4e0928
pothyd <kwonohhyun@gmail.com>
2016-12-04 18:31:00 +0900

    Update restaurant mypage

--- app/src/main/java/hanjo/simukgak/ManageSellRank.java
+++ app/src/main/java/hanjo/simukgak/ManageSellRank.java
@@ -5,17 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class ManageSellRank extends AppCompatActivity implements ManageSellListViewAdapter.ListBtnClickListener {
+public class ManageSellRank extends AppCompatActivity implements ManageSellRankListViewAdapter.ListBtnClickListener {
 
     static final int REQUEST_CODE = 1;
 
-    final ManageSellListViewAdapter adapter = new ManageSellListViewAdapter(this, R.layout.manage_sell_listview_item, this);
+    final ManageSellRankListViewAdapter adapter = new ManageSellRankListViewAdapter(this, R.layout.manage_sell_rank_listview_item, this);
 
     private FileManager fileManager;
 
@@ -27,6 +28,9 @@ public class ManageSellRank extends AppCompatActivity implements ManageSellListV
         listview = (ListView) findViewById(R.id.listorder);
         listview.setAdapter(adapter);
 
+        LinearLayout layout =(LinearLayout)findViewById(R.id.back);
+        layout.setBackgroundResource(R.drawable.bg2);
+
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView parent, View v, int position, long id) {
@@ -35,56 +39,32 @@ public class ManageSellRank extends AppCompatActivity implements ManageSellListV
         });
 
         ArrayList<String> fileValues;
-        ArrayList<String> productList;
         String[] values;
 
         fileManager = new FileManager(getApplicationContext(), "selcomp.txt");
-        fileManager.writeFile("010-9367-7686,공학3동,3,2016.12.05,라면,2000,3");
         fileValues = fileManager.readFile(); //date, location, phoneNum, product1, price1, amount1, product2, price2, amount2,...
 
         for(int i = 0; i < fileValues.size(); i++)
         {
-            productList = new ArrayList<>();
             values = fileValues.get(i).split(",");
-            int[] priceList = new int[(values.length-4)/3];
-            int[] amountList = new int[(values.length-4)/3];
-            for(int j = 3; j < values.length; j = j + 3) {
-                //if()
+            for(int j = 4; j < values.length; j = j + 3) {
+                if(adapter.checkProduct(values[j]) == -1)
+                {
+                    adapter.addItem(values[j], Integer.parseInt(values[j+1]), Integer.parseInt(values[j+2]), 0);
+                }
+                else
+                {
+                    adapter.getItem(adapter.checkProduct(values[j])).increaseNum(Integer.parseInt(values[j+2]));
+                }
 
-
-                productList.add(values[j]);
-                priceList[(j-4)/3] = Integer.parseInt(values[j+1]);
-                amountList[(j-4)/3] = Integer.parseInt(values[j+2]);
             }
-            adapter.addItem(productList, values[3], values[1], values[0]); //
         }
-        int n = checkDue();
-        if(n > 0)
-            Toast.makeText(getApplicationContext(), String.format(Locale.KOREA, "오래된 %d개의 항목이 삭제되었습니다.", n), Toast.LENGTH_SHORT).show();
-        adapter.sortItemByDate();
+        adapter.sortItemByPrice();
 
     }
 
-    @Override
-    protected void onDestroy() {
+    public void onDestroy(View v) {
         super.onDestroy();
-        fileManager.resetData();
-        String data;
-        for(int i = 0; i<adapter.getCount(); i++)
-        {
-            data = adapter.getItem(i).getDate() + "," + adapter.getItem(i).getLocation() + "," + adapter.getItem(i).getPhoneNum();
-            for(int j = 0; j < adapter.getItem(i).getProductList().size(); j++) {
-                data = data + "," + (adapter.getItem(i).getProductList()).get(j);
-                data = data + "," + Integer.toString(adapter.getItem(i).getPrice(j));
-                data = data + "," + Integer.toString(adapter.getItem(i).getAmount(j));
-            }
-            fileManager.writeFile(data);
-        }
-    }
-
-    public void onClickSort(View view)
-    {
-        adapter.sortItemByDate();
     }
 
     @Override
@@ -104,16 +84,5 @@ public class ManageSellRank extends AppCompatActivity implements ManageSellListV
 
             }
         }
-    }
-
-    private int checkDue() {
-        int count = 0;
-        for(int i = 0; i<adapter.getCount(); i++) {
-            if (adapter.checkDate(adapter.getItem(i).getDate())) {
-                adapter.deleteItem(i);
-                count++;
-            }
-        }
-        return count;
     }
 }
--- app/src/main/java/hanjo/simukgak/ManageSellRankItem.java
+++ app/src/main/java/hanjo/simukgak/ManageSellRankItem.java
@@ -9,18 +9,20 @@ import java.util.ArrayList;
 public class ManageSellRankItem {
 
     private String product;
-    private int price;
-    private int num;
-    private int rate;
+    private int price = 0;
+    private int num = 0;
+    private int rate = 0;
 
 
     public void setProduct(String string) {product = string;}
     public void setPrice(int n) {price = n;}
     public void setNum(int n) {num = n;}
+    public void increaseNum(int n) {num = num + n;}
     public void setRate(int n) {rate = n;}
 
     public String getProduct() {return product;}
     public int getPrice() {return price;}
+    public int getTotPrice() {return price * num;}
     public int getNum() {return num;}
     public int getRate() {return rate;}
 }
