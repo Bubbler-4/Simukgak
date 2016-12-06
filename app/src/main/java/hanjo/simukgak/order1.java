307548b239bf9929a06929343c859e080dc3cc8f
Bubbler-4 <bubbler9903@gmail.com>
2016-12-06 20:52:10 +0900

    Add spinning indicator

--- app/src/main/java/hanjo/simukgak/order1.java
+++ app/src/main/java/hanjo/simukgak/order1.java
@@ -1,7 +1,11 @@
 package hanjo.simukgak;
 
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,8 +20,9 @@ import java.util.Observer;
 
 public class order1 extends AppCompatActivity implements Observer {
 
-    static int REQUEST_ACT=0123;
     static final String[] LIST_MENU ={"한식","중식","일식"};
+    private ListView listview;
+    private View mProgressView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +34,10 @@ public class order1 extends AppCompatActivity implements Observer {
 
         ArrayAdapter adapter = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,LIST_MENU);
 
-        ListView listview =(ListView) findViewById(R.id.store);
+        listview =(ListView) findViewById(R.id.store);
         listview.setAdapter(adapter);
+
+        mProgressView = findViewById(R.id.login_progress1);
 
         SocketWrapper.object().deleteObservers();
         SocketWrapper.object().addObserver(this);
@@ -39,31 +46,62 @@ public class order1 extends AppCompatActivity implements Observer {
             @Override
             public void onItemClick(AdapterView parent, View v, int position, long id){
                 SocketWrapper.object().requestRestaurantList(LIST_MENU[position]);
+                showProgress(true);
             }
         });
     }
 
     @Override
     public void update(Observable o, Object arg) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                showProgress(false);
+            }
+        });
+
         SocketWrapper sw = (SocketWrapper) o;
         String[] restaurantList = sw.getRestaurantList();
 
         Intent intent = new Intent(order1.this,order2.class);
         intent.putExtra("restaurantList", restaurantList);
-        startActivityForResult(intent, REQUEST_ACT);
-    }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==REQUEST_ACT)
-        {
-            if(resultCode==RESULT_OK)
-            {
-                setResult(RESULT_OK);
-                finish();
-            }
-        }
+        startActivity(intent);
+        finish();
     }
 
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            listview.setVisibility(show ? View.GONE : View.VISIBLE);
+            listview.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    listview.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            listview.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
 }
--- app/src/main/java/hanjo/simukgak/order2.java
+++ app/src/main/java/hanjo/simukgak/order2.java
@@ -1,6 +1,10 @@
 package hanjo.simukgak;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,9 +19,10 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class order2 extends AppCompatActivity implements Observer {
-static int REQUEST_ACT =1111;
     private String[] restaurantList;
     private String restaurant;
+    private ListView listview;
+    private View mProgressView;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +38,10 @@ static int REQUEST_ACT =1111;
             ArrayAdapter adapter = new ArrayAdapter(
                     this, R.layout.support_simple_spinner_dropdown_item, restaurantList);
 
-            ListView listview =(ListView)findViewById(R.id.koreaList);
+            listview =(ListView)findViewById(R.id.koreaList);
             listview.setAdapter(adapter);
+
+            mProgressView = findViewById(R.id.login_progress2);
 
             SocketWrapper.object().deleteObservers();
             SocketWrapper.object().addObserver(this);
@@ -44,31 +51,63 @@ static int REQUEST_ACT =1111;
                 public void onItemClick(AdapterView parent, View v, int position, long id){
                     restaurant = restaurantList[position];
                     SocketWrapper.object().requestMenuList(restaurant);
+                    showProgress(true);
                 }
             });
         }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==REQUEST_ACT)
-        {
-            if(resultCode==RESULT_OK)
-            {
-                setResult(RESULT_OK);
-                finish();
-            }
-        }
-    }
 
     @Override
     public void update(Observable o, Object arg) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                showProgress(false);
+            }
+        });
+
         SocketWrapper sw = (SocketWrapper) o;
         String menuList = sw.getMenuList();
 
         Intent intent = new Intent(order2.this,order3.class);
         intent.putExtra("StoreName",restaurant);
         intent.putExtra("menuList", menuList);
-        startActivityForResult(intent, REQUEST_ACT);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            listview.setVisibility(show ? View.GONE : View.VISIBLE);
+            listview.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    listview.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            listview.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 }
